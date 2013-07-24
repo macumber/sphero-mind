8dd56bbe0ee60c2c269584ab186cddfb9b01c446
Dan Macumber <dan.macumber@gmail.com>
2013-07-23 20:49:18 -0600

    Got initial commands working

--- test.rb
+++ test.rb
@@ -10,9 +10,27 @@ s.ping
 
 puts "red"
 s.rgb(255, 0, 0)
+sleep(2)
 
 puts "green"
 s.rgb(0, 255, 0)
+sleep(2)
 
 puts "blue"
 s.rgb(0, 0, 255)
+sleep(2)
+
+puts "roll slow"
+s.roll(32, 0, true)
+sleep(2)
+
+puts "roll med"
+s.roll(128, 0, true)
+sleep(2)
+
+puts "roll fast"
+s.roll(255, 0, true)
+sleep(2)
+
+puts "stop"
+s.roll(0, 0, true)
